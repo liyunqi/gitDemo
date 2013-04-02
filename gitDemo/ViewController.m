61652839af67a5a58cf906186d2e25388d357325
Jerry <jerry@JerrytekiMacBook-Pro.local>
2013-04-02 16:59:01 +0800

    to do
    new method to do something

--- gitDemo/ViewController.m
+++ gitDemo/ViewController.m
@@ -19,7 +19,10 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)newMethod
+{
+    // to d0
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
